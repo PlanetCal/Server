c6c0a68a25c987b77c0b5d966e01184ab37883e0
yhawong <yhawong@hotmail.com>
2017-03-15 09:07:00 -0700

    GroupsController

--- src/APIService/routes/groupscontroller.js
+++ src/APIService/routes/groupscontroller.js
@@ -13,35 +13,35 @@ module.exports = function(){
     var endpoint = config.groupsServiceEndpoint;
 
     router.get('/:id', function(req, res){
-        request.get(helpers.getRequestOption(req, config.eventServiceEndpoint + '/groups/' + req.params.id),
+        request.get(helpers.getRequestOption(req, endpoint + '/' + controllerName + '/' + req.params.id),
             function(error, responseFromRequest, body){
                 helpers.handleHttpForwardedResponse(error, responseFromRequest, body, res);
             });    
     });
 
     router.get('/', function(req, res){
-        request.get(helpers.getRequestOption(req, config.eventServiceEndpoint + '/events?' + req.query),
+        request.get(helpers.getRequestOption(req, endpoint + '/' + controllerName + '?' + req.query),
             function(error, responseFromRequest, body){
                 helpers.handleHttpForwardedResponse(error, responseFromRequest, body, res);
             });    
     });
 
     router.post('/', function(req, res) {
-        request.post(helpers.getRequestOption(req, config.eventServiceEndpoint + '/events'),
+        request.post(helpers.getRequestOption(req, endpoint + '/' + controllerName),
             function(error, responseFromRequest, body){
                 helpers.handleHttpForwardedResponse(error, responseFromRequest, body, res);
             });    
     });
 
     router.put('/:id', function(req, res) {
-        request.put(helpers.getRequestOption(req, config.eventServiceEndpoint + '/events/' + req.params.id),
+        request.put(helpers.getRequestOption(req, endpoint + '/' + controllerName + '/' + req.params.id),
             function(error, responseFromRequest, body){
                 helpers.handleHttpForwardedResponse(error, responseFromRequest, body, res);
             });    
     });
 
     router.delete('/:id', function(req, res) {
-        request.delete(helpers.getRequestOption(req, config.eventServiceEndpoint + '/events/' + req.params.id),
+        request.delete(helpers.getRequestOption(req, endpoint + '/' + controllerName + '/' + req.params.id),
             function(error, responseFromRequest, body){
                 helpers.handleResponse(error, responseFromRequest, body, res);
             });    
