8665cc622fc2f17de68f04525743cd6162054a9b
yhawong <yhawong@hotmail.com>
2017-04-02 13:21:08 -0700

    Use npm install instead of npm update

--- src/start-services.cmd
+++ src/start-services.cmd
@@ -1,7 +1,9 @@
-start cmd.exe /k "cd\ & cd %~dp0\APIService & npm update & echo %~dp0 & node.exe app.js"
+start cmd.exe /k "cd\ & cd %~dp0\APIService & npm install & echo %~dp0 & node.exe app.js"
 
-start cmd.exe /k "cd\ & cd %~dp0\UserAuthService & npm update & echo %~dp0 & node.exe app.js"
+start cmd.exe /k "cd\ & cd %~dp0\UserAuthService & npm install & echo %~dp0 & node.exe app.js"
 
-start cmd.exe /k "cd\ & cd %~dp0\UserDetailsService & npm update & echo %~dp0 & node.exe app.js"
+start cmd.exe /k "cd\ & cd %~dp0\UserDetailsService & npm install & echo %~dp0 & node.exe app.js"
 
-start cmd.exe /k "cd\ & cd %~dp0\EventsService & npm update & echo %~dp0 & node.exe app.js"
+start cmd.exe /k "cd\ & cd %~dp0\EventsService & npm install & echo %~dp0 & node.exe app.js"
+
+start cmd.exe /k "cd\ & cd %~dp0\GroupsService & npm install & echo %~dp0 & node.exe app.js"
