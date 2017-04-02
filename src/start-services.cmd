f87bc22c2cd6832e7fe82cf5da0cc8be1dc782f4
khatauli <sachin.kumar.v@gmail.com>
2017-04-02 14:23:50 -0700

    Updated start-services.cmd

--- src/start-services.cmd
+++ src/start-services.cmd
@@ -1,9 +1,9 @@
-start cmd.exe /k "cd\ & cd %~dp0\APIService & npm install & echo %~dp0 & node.exe app.js"
+start cmd.exe /k "cd %~dp0\APIService & npm install & title APIService & node.exe app.js"
 
-start cmd.exe /k "cd\ & cd %~dp0\UserAuthService & npm install & echo %~dp0 & node.exe app.js"
+start cmd.exe /k "cd %~dp0\UserAuthService & npm install & title UserAuthService & node.exe app.js"
 
-start cmd.exe /k "cd\ & cd %~dp0\UserDetailsService & npm install & echo %~dp0 & node.exe app.js"
+start cmd.exe /k "cd %~dp0\UserDetailsService & npm install & title UserDetailsService & node.exe app.js"
 
-start cmd.exe /k "cd\ & cd %~dp0\EventsService & npm install & echo %~dp0 & node.exe app.js"
+start cmd.exe /k "cd %~dp0\EventsService & npm install & title EventsService & node.exe app.js"
 
-start cmd.exe /k "cd\ & cd %~dp0\GroupsService & npm install & echo %~dp0 & node.exe app.js"
+start cmd.exe /k "cd %~dp0\GroupsService & npm install & title GroupsService & node.exe app.js"
