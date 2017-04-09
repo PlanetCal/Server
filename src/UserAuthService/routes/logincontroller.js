c32b3e5dfa3aef7b03a0bb840dd6cf25622b06e4
yhawong <yhawong@hotmail.com>
2017-04-09 00:55:23 -0700

    Add back name support when user logs on

--- src/UserAuthService/routes/logincontroller.js
+++ src/UserAuthService/routes/logincontroller.js
@@ -8,13 +8,14 @@ var ForbiddenException = require('../../common/error.js').ForbiddenException;
 module.exports = function(passport){
 
     router.post('/', passport.authenticate('local'), helpers.wrap(function *(req, res){
-        if (req.user && req.user.email && req.user.id ){
+        console.log('user: ' + JSON.stringify(req.user));
+        if (req.user && req.user.email && req.user.id && req.user.name){
             var tokenGenerator = new TokenGenerator();
-            var token = tokenGenerator.encode({ email : req.user.email, id : req.user.id, time : Date.now() });
-            res.status(200).json({ token : token, id : req.user.id });
+            var token = tokenGenerator.encode({ email : req.user.email, id : req.user.id, name : req.user.name, time : Date.now() });
+            res.status(200).json({ token : token, id : req.user.id, name : req.user.name });
         }
         else{
-            throw new ForbiddenException('Forbidden.');
+            throw new ForbiddenException('Forbidden. Please make sure that user has email, id and name.');
         }   
     }));
 
