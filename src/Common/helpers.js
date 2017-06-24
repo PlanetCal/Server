5778bba5a7b2b6883b01d3caa9934e2c373a750b
khatauli <sachin.kumar.v@gmail.com>
2017-06-24 11:40:31 -0700

    Adding Email method.

--- src/Common/helpers.js
+++ src/Common/helpers.js
@@ -2,6 +2,8 @@
 
 var Promise = require('bluebird');
 var request = require('request-promise');
+var email = require("./node_modules/emailjs/email");
+var emailConstants = require('./constants.json')['emailConstants'];
 var ForbiddenException = require('./error.js').ForbiddenException;
 var UnauthorizedException = require('./error.js').UnauthorizedException;
 var InternalServerException = require('./error.js').InternalServerException;
@@ -9,42 +11,44 @@ var HttpRequestException = require('./error.js').HttpRequestException;
 
 module.exports = {
 
-    'wrap' : function wrap (genFn) { 
+    'wrap': function wrap(genFn) {
         var cr = Promise.coroutine(genFn);
         return function (req, res, errorHandler) {
             cr(req, res, errorHandler).catch(errorHandler);
         }
     },
 
-    'wrapLocalStrategyAuth' : function wrapLocalStrategyAuth(genFunc){
-        var cr = Promise.coroutine(genFunc); 
+    'wrapLocalStrategyAuth': function wrapLocalStrategyAuth(genFunc) {
+        var cr = Promise.coroutine(genFunc);
         return function (req, email, password, done) {
-            cr(req, email, password, done).catch(function(err){
+            cr(req, email, password, done).catch(function (err) {
                 done(err, null);
             });
         }
     },
 
-    'getRequestOption' : function getRequestOption(req, targetEndpoint, method){
+    'getRequestOption': function getRequestOption(req, targetEndpoint, method) {
         return {
-            method : method,
-            headers: {'content-type' : 'application/json; charset=utf-8',
-                      'auth-identity' : req.headers['auth-identity'],
-                      'version' : req.headers['version'],
-                      'activityid' : req.headers['activityid']},
+            method: method,
+            headers: {
+                'content-type': 'application/json; charset=utf-8',
+                'auth-identity': req.headers['auth-identity'],
+                'version': req.headers['version'],
+                'activityid': req.headers['activityid']
+            },
             url: targetEndpoint,
             body: JSON.stringify(req.body)
         };
     },
 
-    'removeDuplicatedItemsById' : function removeDuplicatedItemsById(results){
-        if (results){
+    'removeDuplicatedItemsById': function removeDuplicatedItemsById(results) {
+        if (results) {
             var filteredResults = {};
 
             // de-dupe uisng dictionary
-            for (var i in results){
+            for (var i in results) {
                 var obj = results[i];
-                if (!filteredResults.hasOwnProperty(obj.id)){
+                if (!filteredResults.hasOwnProperty(obj.id)) {
                     filteredResults[obj.id] = obj;
                 }
             }
@@ -54,36 +58,36 @@ module.exports = {
         return [];
     },
 
-    'convertFieldSelectionToConstraints' : function convertFieldSelectionToConstraints(prefix, fields){
-        if (fields){
+    'convertFieldSelectionToConstraints': function convertFieldSelectionToConstraints(prefix, fields) {
+        if (fields) {
             var arr = [];
-            for(var i in fields){
+            for (var i in fields) {
                 arr.push(prefix + '.' + fields[i]);
             }
 
-            if (arr.length > 0){
+            if (arr.length > 0) {
                 return ',' + arr.join(', ');
             }
         }
         return '';
     },
 
-    'constructResponseJsonFromExceptionRecursive' : function constructResponseJsonFromExceptionRecursive(exceptionObject, logStack){
+    'constructResponseJsonFromExceptionRecursive': function constructResponseJsonFromExceptionRecursive(exceptionObject, logStack) {
         var returnedJson;
-        if (exceptionObject){
-            returnedJson = 
-                { 
-                    name : exceptionObject.name, 
-                    message : exceptionObject.message,
-                    code : exceptionObject.code,
-                    activityId : exceptionObject.activityId,
-                    innerException : constructResponseJsonFromExceptionRecursive(exceptionObject.innerException),
-                    serviceName : exceptionObject.serviceName,
-                    errorcode : exceptionObject.errorcode,
-                    url : exceptionObject.url                    
+        if (exceptionObject) {
+            returnedJson =
+                {
+                    name: exceptionObject.name,
+                    message: exceptionObject.message,
+                    code: exceptionObject.code,
+                    activityId: exceptionObject.activityId,
+                    innerException: constructResponseJsonFromExceptionRecursive(exceptionObject.innerException),
+                    serviceName: exceptionObject.serviceName,
+                    errorcode: exceptionObject.errorcode,
+                    url: exceptionObject.url
                 };
 
-            if (logStack === true){
+            if (logStack === true) {
                 returnedJson.stack = exceptionObject.stack;
             }
         }
@@ -91,32 +95,58 @@ module.exports = {
         return returnedJson;
     },
 
-    'forwardHttpRequest' : function *forwardHttpRequest(options, serviceName){
-        return yield request(options).catch(function(err){
-            try{
+    'forwardHttpRequest': function* forwardHttpRequest(options, serviceName) {
+        return yield request(options).catch(function (err) {
+            try {
                 var extractedException = JSON.parse(err.error);
             }
-            catch(e){
+            catch (e) {
             }
 
-            if (!extractedException){
+            if (!extractedException) {
                 extractedException = err;
             }
             throw new HttpRequestException('Request to ' + serviceName + ' failed.', options.url, extractedException);
         });
     },
 
-    'handleServiceException' : function handleServiceException(err, req, res, serviceName, logger, logStack){
+    // Documentation: https://github.com/eleith/emailjs    
+    'sendEmail': function sendEmail(logger, toAddress, subject, messageText, messageAlternateHtmlText) {
+        var server = email.server.connect({
+            user: emailConstants.adminName,
+            password: emailConstants.adminPassword,
+            host: emailConstants.smtpHost,
+            tls: { ciphers: emailConstants.smtpCiphers }
+        });
+
+        var message = {
+            from: emailConstants.fromEmailAddress,
+            text: messageText,
+            to: toAddress,
+            subject: subject,
+            attachment:
+            [
+                { data: messageAlternateHtmlText, alternative: true }
+            ]
+        };
+
+        // send the message and get a callback with an error or details of the message that was sent
+        server.send(message, function (err, message) {
+            logger.get().info(err || message);
+        });
+    },
+
+    'handleServiceException': function handleServiceException(err, req, res, serviceName, logger, logStack) {
         err.serviceName = serviceName;
         err.activityId = req.headers['activityid'];
 
         var exception = this.constructResponseJsonFromExceptionRecursive(err, logStack);
-        
-        if (err && err.code < 500){
-            logger.get().info({exception : exception});
+
+        if (err && err.code < 500) {
+            logger.get().info({ exception: exception });
         }
-        else{        
-            logger.get().error({exception : exception});
+        else {
+            logger.get().error({ exception: exception });
         }
 
         res.status(err.code || 500).json(exception);
--- src/UserAuthService/routes/userauthcontroller.js
+++ src/UserAuthService/routes/userauthcontroller.js
@@ -1,6 +1,6 @@
 'use strict'
 
-module.exports = function(passport, config, logger){
+module.exports = function (passport, config, logger) {
 
     var router = require('express').Router();
     var PasswordCrypto = require('../passwordcrypto.js').PasswordCrypto;
@@ -22,55 +22,57 @@ module.exports = function(passport, config, logger){
 
     bodyschemavalidator.addSchema(userauthschema, 'userauth');
 
-    router.post('/', bodyschemavalidator.validateSchema('userauth'), helpers.wrap(function *(req, res){
-        logger.get().debug({req : req}, 'Creating userAuth object...');
+    router.post('/', bodyschemavalidator.validateSchema('userauth'), helpers.wrap(function* (req, res) {
+        logger.get().debug({ req: req }, 'Creating userAuth object...');
 
         var passwordCrypto = new PasswordCrypto();
         var passwordHash = passwordCrypto.generateHash(req.body.password);
-        var options = { preTriggerInclude: config.insertUniqueUserTriggerName };   
+        var options = { preTriggerInclude: config.insertUniqueUserTriggerName };
 
-        var documentResponse = yield dal.insertAsync({ email: req.body.email, passwordHash: passwordHash, name : req.body.name, firstTimeLogon : true }, options);
+        //helpers.sendEmail(logger, 'sachin kumar <sachin_kumar3@hotmail.com>', "sending email", 'this is great', '<html>is it not great</html>');
 
-        logger.get().debug({req : req, userAuth : documentResponse.resource }, 'userAuth object created successfully.');
+        var documentResponse = yield dal.insertAsync({ email: req.body.email, passwordHash: passwordHash, name: req.body.name, firstTimeLogon: true }, options);
 
-        res.status(201).json({ email : documentResponse.resource.email, id : documentResponse.resource.id, name : documentResponse.resource.name, firstTimeLogon : true });                
+        logger.get().debug({ req: req, userAuth: documentResponse.resource }, 'userAuth object created successfully.');
+
+        res.status(201).json({ email: documentResponse.resource.email, id: documentResponse.resource.id, name: documentResponse.resource.name, firstTimeLogon: true });
     }));
 
-    router.put('/:id', helpers.wrap(function *(req, res){
-        if (!req.body.email || !req.body.password || !req.body.name){
+    router.put('/:id', helpers.wrap(function* (req, res) {
+        if (!req.body.email || !req.body.password || !req.body.name) {
             throw new BadRequestException('Cannot find email, password or name in body.', errorcode.EmailOrPasswordNotFoundInBody);
         }
-        else if (!isOperationAuthorized(req)){
-            throw new ForbiddenException('Forbidden.');
-        }
-        else{
-            logger.get().debug({req : req}, 'Updating userAuth object...');
-            var passwordCrypto = new PasswordCrypto();
-            var passwordHash = passwordCrypto.generateHash(req.body.password);
-
-            var documentResponse = yield dal.updateAsync(req.params.id, { email: req.body.email, passwordHash: passwordHash, id: req.params.id, name : req.body.name });
-
-            logger.get().debug({req : req, userAuth: documentResponse.resource}, 'userAuth object updated successfully.');
-
-            res.status(200).json({ id : documentResponse.resource.id });
-        }
-    }));
-
-    router.delete('/:id', helpers.wrap(function *(req, res){
-        //all small letters even if header name has capitals
-        if (!isOperationAuthorized(req)){
+        else if (!isOperationAuthorized(req)) {
             throw new ForbiddenException('Forbidden.');
         }
         else {
-            logger.get().debug({req : req}, 'Deleting userAuth object...');
-            var documentResponse = yield dal.removeAsync(req.params.id);
-            logger.get().debug({req : req}, 'userAuth object deleted successfully.');
-            res.status(200).json({ id : req.params.id });
+            logger.get().debug({ req: req }, 'Updating userAuth object...');
+            var passwordCrypto = new PasswordCrypto();
+            var passwordHash = passwordCrypto.generateHash(req.body.password);
+
+            var documentResponse = yield dal.updateAsync(req.params.id, { email: req.body.email, passwordHash: passwordHash, id: req.params.id, name: req.body.name });
+
+            logger.get().debug({ req: req, userAuth: documentResponse.resource }, 'userAuth object updated successfully.');
+
+            res.status(200).json({ id: documentResponse.resource.id });
         }
     }));
-    return router;  
+
+    router.delete('/:id', helpers.wrap(function* (req, res) {
+        //all small letters even if header name has capitals
+        if (!isOperationAuthorized(req)) {
+            throw new ForbiddenException('Forbidden.');
+        }
+        else {
+            logger.get().debug({ req: req }, 'Deleting userAuth object...');
+            var documentResponse = yield dal.removeAsync(req.params.id);
+            logger.get().debug({ req: req }, 'userAuth object deleted successfully.');
+            res.status(200).json({ id: req.params.id });
+        }
+    }));
+    return router;
 }
 
-function isOperationAuthorized(req){
+function isOperationAuthorized(req) {
     return req.headers['auth-identity'] === req.params.id;
 }
