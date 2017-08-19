886843065809b1be5fbb05e9699b9b133b8dde43
khatauli <sachin.kumar.v@gmail.com>
2017-08-19 11:33:33 -0700

    Khatauli/events get by date (#10)
    * Fixed a bug in helpers code
    
    * Added time filtering for events
    
    * Sorted events based on dates.

--- src/EventsService/routes/eventscontroller.js
+++ src/EventsService/routes/eventscontroller.js
@@ -38,21 +38,30 @@ module.exports = function (config, logger) {
             groupids = req.query.groupids.split('|');
         }
 
+        var filterExpression = req.query.filter;
+
         var documentResponse;
-        if (!groupids && !fields) {
+        if (!groupids && !fields && !filterExpression) {
             logger.get().debug({ req: req }, 'Retrieving all event objects...');
             documentResponse = yield findAllEvents(dal);
         }
         else {
             logger.get().debug({ req: req }, 'Retrieving event objects by ids...');
-            documentResponse = yield findEventsByGroupsIdsAsync(dal, groupids, fields);
+            documentResponse = yield findEventsByGroupsIdsAsync(dal, groupids, fields, filterExpression);
         }
 
         var results = documentResponse.feed;
-        var filteredResults = helpers.removeDuplicatedItemsById(results);
+        //var results = helpers.removeDuplicatedItemsById(results);
 
-        logger.get().debug({ req: req, events: filteredResults }, 'Event objects retrieved successfully.');
-        res.status(200).json(filteredResults);
+        // the following will only sort if startDateTime is part of the queried fields.         
+        results.sort(function (a, b) {
+            a = new Date(a.startDateTime);
+            b = new Date(b.startDateTime);
+            return a < b ? -1 : a > b ? 1 : 0;
+        });
+
+        logger.get().debug({ req: req, events: results }, 'Event objects retrieved successfully.');
+        res.status(200).json(results);
     }));
 
     router.put('/:id', helpers.wrap(function* (req, res) {
@@ -84,7 +93,7 @@ module.exports = function (config, logger) {
 
         var groups = req.body.groups;
         logger.get().debug({ req: req }, 'Retrieving event objects by ids...');
-        var documentResponse = yield findEventsByGroupsIdsAsync(dal, groups, null);
+        var documentResponse = yield findEventsByGroupsIdsAsync(dal, groups, null, null);
 
         var results = documentResponse.feed;
         var filteredResults = helpers.removeDuplicatedItemsById(results);
@@ -123,18 +132,28 @@ module.exports = function (config, logger) {
         return dal.getAsync(querySpec);
     }
 
-    function findEventsByGroupsIdsAsync(dal, groupIds, fields) {
-        var constraints = helpers.convertFieldSelectionToConstraints('e', fields);
+    function findEventsByGroupsIdsAsync(dal, groupIds, fields, filterExpression) {
 
-        var parameters = [{
+        if (!groupIds || groupIds.length === 0) {
+            throw new BadRequestException('The query should specify the group Ids for which we need to get events', errorcode.GroupIdNotFoundInPayload);
+        }
+
+        var constraints = helpers.convertFieldSelectionToConstraints('e', fields);
+        var filterExpressionParsed = helpers.convertFilterExpressionToParameters('e', filterExpression, 'AND', '');
+
+        var queryStatement = "SELECT e.id" + constraints + " FROM e JOIN g IN e.groups WHERE ARRAY_CONTAINS(@groupsIds, g) " + filterExpressionParsed.filterExpression;
+        var parameters = filterExpressionParsed.parameters;
+
+        parameters.push({
             name: "@groupsIds",
             value: groupIds
-        }];
+        });
 
         var querySpec = {
-            query: "SELECT e.id" + constraints + " FROM e JOIN g IN e.groups WHERE ARRAY_CONTAINS(@groupsIds, g)",
+            query: queryStatement,
             parameters: parameters
         };
+
         return dal.getAsync(querySpec);
     }
 
